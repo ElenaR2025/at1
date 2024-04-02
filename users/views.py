dc7c36d076f6f5f987930f22fc7f853fb7ae6430
unknown <elena.russell@outlook.com>
2024-04-02 16:12:32 +1100

    Version 8.LoginpageUpdate

--- users/views.py
+++ users/views.py
@@ -8,7 +8,7 @@ from django.contrib import messages
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("users:login"))
-    return render(request, "eduprod/Test2.html")
+    return render(request, "eduprod/Home.html")
 
 def login_view(request):
     if request.method == "POST":
@@ -17,7 +17,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect(reverse("eduprod:Test2"))
+            return HttpResponseRedirect(reverse("eduprod:Home"))
         else:
             messages.success(request, "Invalid Credentials.")
             return redirect(reverse('users:login'))
@@ -28,3 +28,6 @@ def logout_view(request):
     logout(request)
     messages.success(request, "Successfully logged out.")
     return redirect(reverse('users:login'))
+
+def layout(request):
+    return render(request, 'users/login.html')
